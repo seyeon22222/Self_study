6bec37d1d0a2837aefd7827912476f1765d7e9d6
seyeon22222 <92151066+seyeon22222@users.noreply.github.com>
2023-03-16 23:47:01 +0900

    array

--- array/room_num.py
+++ array/room_num.py
@@ -2,8 +2,12 @@ room_num = input()
 res = [0] * 10
 for i in range(0, len(room_num)):
     res_num = int(room_num[i])
-    if (res_num == 6):
-        res[9] += 1
-    res[res_num] += 1
+    if (res_num == 6 or res_num == 9):
+        if (res[6] == res[9]):
+            res[9] += 1
+        else:
+            res[6] += 1
+    else:
+        res[res_num] += 1
 
 print(max(res))
