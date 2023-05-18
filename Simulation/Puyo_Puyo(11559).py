31a3fe6d51da804e18835413a3a02777575383eb
seyeon22222 <92151066+seyeon22222@users.noreply.github.com>
2023-05-18 21:44:00 +0900

    Simulation

--- Simulation/Puyo_Puyo(11559).py
+++ Simulation/Puyo_Puyo(11559).py
@@ -4,26 +4,30 @@ dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
 # 탐색하는 함수
-def bfs(x, y):
-    q = deque()
+def bfs(x, y, state):
     q.append((x, y))
     visited[x][y] = 1
-    temp.append((x, y))
+    cnt = 1
     while q:
         x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < 12 and 0 <= ny < 6:
-                if not visited[nx][ny] and arr[nx][ny] == arr[x][y]:
+                if not visited[nx][ny] and arr[nx][ny] == state:
                     q.append((nx, ny))
                     visited[nx][ny] = 1
-                    temp.append((nx,ny))
+                    cnt += 1
+    if cnt >= 4:
+        return state
+    return None
 
 # 4개 이상 일 때, 터트리는 함수
-def bang(temp):
-    for i, j in temp:
-        arr[i][j] = '.'
+def bang(state):
+    for i in range(12):
+        for j in range(6):
+            if visited[i][j] == 1 and arr[i][j] == state:
+                arr[i][j] = '.'
 
 # 터트린 후, 아래로 내리는 함수
 def re_arr():
@@ -39,15 +43,16 @@ def re_arr():
 arr = [list(input()) for _ in range(12)]
 ans = 0
 while True:
-    visited = [[0] * 6 for _ in range(12)]
+
+    q = deque()
     check = 0
     for i in range(12):
         for j in range(6):
-            if arr[i][j] != '.' and not visited[i][j]:
-                temp = []
-                bfs(i, j)
-                if len(temp) >= 4:
-                    bang(temp)
+            if arr[i][j] != '.':
+                visited = [[0] * 6 for _ in range(12)]
+                state = bfs(i, j, arr[i][j])
+                if state is not None:
+                    bang(state)
                     check = 1
     if check == 0:
         break
